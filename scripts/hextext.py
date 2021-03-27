37cf8289f07e2ad0e5168ed1732043bc42e06a8e
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-03-27 16:02:45 -0400

    Create hextext.py

---
+++ scripts/hextext.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def hextext(n):
+    print(bytearray.fromhex(n).decode())
+
+n = input("Enter the hexadecimal value for conversion to text value: ")
+
+hextext(n)
