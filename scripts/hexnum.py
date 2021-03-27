ea55d9ce53bb86393e8dee3bbfe925125bb04a01
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-03-27 16:04:45 -0400

    Create hexnum.py

---
+++ scripts/hexnum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import re
+
+
+h = input("Enter hex value for conversion: ")
+def main(h):
+    dec = int(h, 16);
+    print(h,"in Decimal =",str(dec));
+
+main(h)
